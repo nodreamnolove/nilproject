847d283f2e2a154d069fd59cda66a92b15f00758
IOS developer <hmenghu@qq.com>
2015-02-13 21:33:29 +0800

    Initial Commit

---
+++ nilproject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nilproject
+//
+//  Created by mac on 15/2/13.
+//  Copyright (c) 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ nilproject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  nilproject
+//
+//  Created by mac on 15/2/13.
+//  Copyright (c) 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
